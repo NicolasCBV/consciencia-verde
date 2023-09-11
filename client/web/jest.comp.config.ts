eb3c6c90932c6336ae99062b656678d9b5d41692
NicolasCBV <nicolascleiton700e@gmail.com>
2023-09-10 23:50:13 -0300

    force action usage

--- client/web/jest.comp.config.ts
+++ client/web/jest.comp.config.ts
@@ -1,27 +1,27 @@
 import nextJest from "next/jest";
-import { Config } from 'jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
 
 const createJestConfig = nextJest({
   dir: "./"
 });
 
 const config: Config = {
-  moduleFileExtensions: ['js', 'json', 'tsx', 'ts'],
-  testRegex: '.*\\.spec\\.(ts|js)x$',
+  moduleFileExtensions: ["js", "json", "tsx", "ts"],
+  testRegex: ".*\\.spec\\.(ts|js)x$",
   transform: {
-    '^.+\\.(ts|js)x$': 'ts-jest',
+    "^.+\\.(ts|js)x$": "ts-jest",
   },
   clearMocks: true,
-  collectCoverageFrom: ['**/*.(ts|js)x'],
-  coverageDirectory: 'coverage',
+  collectCoverageFrom: ["**/*.(ts|js)x"],
+  coverageDirectory: "coverage",
 
   setupFiles: ["./tests/setup/reflect-metadata.ts"],
   setupFilesAfterEnv: ["./tests/setup/react.ts"],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
+    prefix: "<rootDir>/",
   }),
 };
 
